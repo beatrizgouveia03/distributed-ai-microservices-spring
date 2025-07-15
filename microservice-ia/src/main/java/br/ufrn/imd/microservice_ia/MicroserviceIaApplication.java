eefe2f3684aceb47e9eaf3fee3265a13a960eee9
Beatriz <beatrizgouveia.gadelha0@gmail.com>
2025-07-15 10:11:13 -0300

    feat: added dependency and config to use prometheus

--- microservice-ia/src/main/java/br/ufrn/imd/microservice_ia/MicroserviceIaApplication.java
+++ microservice-ia/src/main/java/br/ufrn/imd/microservice_ia/MicroserviceIaApplication.java
@@ -2,7 +2,9 @@ package br.ufrn.imd.microservice_ia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceIaApplication {
 
